668c14312d63ca087d3f791e5833d8d0c800a4b1
rey-1e <kanaderaj1216@gmail.com>
2026-08-05 13:21:14 +0530

    created a new react-app in the current folder using vite toolchain
    npm create vite@latest . -- --template react

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+function FavFood() {
+
+    return (             
+        <ul>             
+                <li>ABCD</li>
+                <li>EFGH</li>
+                <li>IJKL</li>
+                <li>MNOP</li>
+        </ul>             
+    )
+
+}
+
+export default FavFo
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Greeting from './Greetings.jsx'
+import FavFood from './List.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Greeting />
+    <FavFood />
+  </StrictMode>,
+)
